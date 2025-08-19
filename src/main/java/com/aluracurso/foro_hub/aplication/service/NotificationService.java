b360c09d7bae43220c5efd30307094995d9ce595
Christopher Schiefelbein <ch.schiefelbein@duocuc.cl>
2025-08-19 17:49:21 -0400

    Desafio alura

---
+++ src/main/java/com/aluracurso/foro_hub/aplication/service/NotificationService.java
@@ -0,0 +1,16 @@
+package com.aluracurso.foro_hub.aplication.service;
+
+import com.aluracurso.foro_hub.domain.topico.Topico;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public void notifyNewTopic(Topico topico) {
+        System.out.println("Notificando sobre el nuevo tópico: " + topico.getTitulo());
+        // Aquí podrías agregar la lógica real para enviar un correo electrónico,
+        // Por ejemplo:
+        // emailService.sendEmail(topico.getAutor(), "Nuevo Tópico creado", "Se ha creado el tópico: " + topico.getTitulo());
+    }
+}
+
